5724965d6c077a1b81a0265911e0b3313b81cd74
Tolga <tolgasahin61000@gmail.com>
2026-04-09 23:45:34 +0200

    Add tag file

---
+++ demo.ts
@@ -0,0 +1,7 @@
+/**
+ * PDFForge
+ *
+ * Looking for contact, setup instructions or features?
+ * → Everything is available in the README.md
+ * (This file is only to tag the project as TypeScript on Git)
+ */
